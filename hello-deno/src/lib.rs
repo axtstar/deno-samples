8a2cd5a8a7d6651f9dd7d224fbf787bb384fdd2c
axt <axt_star@hotmail.com>
2020-07-05 23:00:14 +0900

    fix

--- hello-deno/src/lib.rs
+++ hello-deno/src/lib.rs
@@ -2,3 +2,12 @@
 pub extern "C" fn square(x: u32) -> u32 {
     x * x
 }
+
+#[no_mangle]
+pub fn fib(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fib(n-2) + fib(n-1),
+    }
+}
--- hello-deno/wasm.ts
+++ hello-deno/wasm.ts
@@ -3,9 +3,12 @@ const wasmModule = new WebAssembly.Module(wasmCode);
 const wasmInstance = new WebAssembly.Instance(wasmModule);
 const {
     square,
+    fib
 } = wasmInstance.exports;
 
-console.log(square(1));
-console.log(square(2));
-console.log(square(3));
-console.log(square(4));
+console.log(`square(1) : ${square(1)}`);
+console.log(`square(2) : ${square(2)}`);
+console.log(`square(3) : ${square(3)}`);
+console.log(`square(5) : ${square(5)}`);
+
+console.log(`fib(5) : ${fib(10)}`);
